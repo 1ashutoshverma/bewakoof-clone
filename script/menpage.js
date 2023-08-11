6139445003258f8c9fd2e63640f09e7c8e270e1e
Ashutosh Verma <ashu9236047613@gmail.com>
2023-08-11 12:17:04 +0530

    men page created

---
+++ script/menpage.js
@@ -0,0 +1,331 @@
+var menPageData = [
+  {
+    img: "https://images.bewakoof.com/t640/conquer-strip-half-sleeve-t-shirt-black-300965-1656141795-1.jpg",
+    brand: "Bewakoof",
+    name: "Conquer Strip Half Sleeve T-Shirts Black",
+    price: "499",
+    disPrice: "299",
+    color: "black",
+    pagelink: "#",
+  },
+  //   {
+  //     img: "",
+  //     brand: "",
+  //     name: "",
+  //     price: "",
+  //     disPrice: "",
+  //     color: "",
+  //   },
+  {
+    img: "https://images.bewakoof.com/t640/conquer-strip-half-sleeve-t-shirt-black-300965-1656141795-1.jpg",
+    brand: "Bewakoof",
+    name: "Conquer Strip Half Sleeve T-Shirts Black",
+    price: "499",
+    disPrice: "299",
+    color: "black",
+    pagelink: "#",
+  },
+  {
+    img: "https://images.bewakoof.com/t640/conquer-strip-half-sleeve-t-shirt-black-300965-1656141795-1.jpg",
+    brand: "Bewakoof",
+    name: "Conquer Strip Half Sleeve T-Shirts Black",
+    price: "499",
+    disPrice: "299",
+    color: "black",
+    pagelink: "#",
+  },
+  {
+    img: "https://images.bewakoof.com/t640/conquer-strip-half-sleeve-t-shirt-black-300965-1656141795-1.jpg",
+    brand: "Bewakoof",
+    name: "Conquer Strip Half Sleeve T-Shirts Black",
+    price: "499",
+    disPrice: "299",
+    color: "black",
+    pagelink: "#",
+  },
+  {
+    img: "https://images.bewakoof.com/t640/conquer-strip-half-sleeve-t-shirt-black-300965-1656141795-1.jpg",
+    brand: "Bewakoof",
+    name: "Conquer Strip Half Sleeve T-Shirts Black",
+    price: "499",
+    disPrice: "299",
+    color: "black",
+    pagelink: "#",
+  },
+  {
+    img: "https://images.bewakoof.com/t640/conquer-strip-half-sleeve-t-shirt-black-300965-1656141795-1.jpg",
+    brand: "Bewakoof",
+    name: "Conquer Strip Half Sleeve T-Shirts Black",
+    price: "499",
+    disPrice: "299",
+    color: "black",
+    pagelink: "#",
+  },
+  {
+    img: "https://images.bewakoof.com/t640/conquer-strip-half-sleeve-t-shirt-black-300965-1656141795-1.jpg",
+    brand: "Bewakoof",
+    name: "Conquer Strip Half Sleeve T-Shirts Black",
+    price: "499",
+    disPrice: "299",
+    color: "black",
+    pagelink: "#",
+  },
+  {
+    img: "https://images.bewakoof.com/t640/conquer-strip-half-sleeve-t-shirt-black-300965-1656141795-1.jpg",
+    brand: "Bewakoof",
+    name: "Conquer Strip Half Sleeve T-Shirts Black",
+    price: "499",
+    disPrice: "299",
+    color: "black",
+    pagelink: "#",
+  },
+  {
+    img: "https://images.bewakoof.com/t640/conquer-strip-half-sleeve-t-shirt-black-300965-1656141795-1.jpg",
+    brand: "Bewakoof",
+    name: "Conquer Strip Half Sleeve T-Shirts Black",
+    price: "499",
+    disPrice: "299",
+    color: "black",
+    pagelink: "#",
+  },
+  {
+    img: "https://images.bewakoof.com/t640/conquer-strip-half-sleeve-t-shirt-black-300965-1656141795-1.jpg",
+    brand: "Bewakoof",
+    name: "Conquer Strip Half Sleeve T-Shirts Black",
+    price: "499",
+    disPrice: "299",
+    color: "black",
+    pagelink: "#",
+  },
+  {
+    img: "https://images.bewakoof.com/t640/conquer-strip-half-sleeve-t-shirt-black-300965-1656141795-1.jpg",
+    brand: "Bewakoof",
+    name: "Conquer Strip Half Sleeve T-Shirts Black",
+    price: "499",
+    disPrice: "299",
+    color: "black",
+    pagelink: "#",
+  },
+  {
+    img: "https://images.bewakoof.com/t640/conquer-strip-half-sleeve-t-shirt-black-300965-1656141795-1.jpg",
+    brand: "Bewakoof",
+    name: "Conquer Strip Half Sleeve T-Shirts Black",
+    price: "499",
+    disPrice: "299",
+    color: "black",
+    pagelink: "#",
+  },
+  {
+    img: "https://images.bewakoof.com/t640/conquer-strip-half-sleeve-t-shirt-black-300965-1656141795-1.jpg",
+    brand: "Bewakoof",
+    name: "Conquer Strip Half Sleeve T-Shirts Black",
+    price: "499",
+    disPrice: "299",
+    color: "black",
+    pagelink: "#",
+  },
+  {
+    img: "https://images.bewakoof.com/t640/conquer-strip-half-sleeve-t-shirt-black-300965-1656141795-1.jpg",
+    brand: "Bewakoof",
+    name: "Conquer Strip Half Sleeve T-Shirts Black",
+    price: "499",
+    disPrice: "299",
+    color: "black",
+    pagelink: "#",
+  },
+  {
+    img: "https://images.bewakoof.com/t640/conquer-strip-half-sleeve-t-shirt-black-300965-1656141795-1.jpg",
+    brand: "Bewakoof",
+    name: "Conquer Strip Half Sleeve T-Shirts Black",
+    price: "499",
+    disPrice: "299",
+    color: "black",
+    pagelink: "#",
+  },
+  {
+    img: "https://images.bewakoof.com/t640/conquer-strip-half-sleeve-t-shirt-black-300965-1656141795-1.jpg",
+    brand: "Bewakoof",
+    name: "Conquer Strip Half Sleeve T-Shirts Black",
+    price: "499",
+    disPrice: "299",
+    color: "black",
+    pagelink: "#",
+  },
+  {
+    img: "https://images.bewakoof.com/t640/conquer-strip-half-sleeve-t-shirt-black-300965-1656141795-1.jpg",
+    brand: "Bewakoof",
+    name: "Conquer Strip Half Sleeve T-Shirts Black",
+    price: "499",
+    disPrice: "299",
+    color: "black",
+    pagelink: "#",
+  },
+  {
+    img: "https://images.bewakoof.com/t640/conquer-strip-half-sleeve-t-shirt-black-300965-1656141795-1.jpg",
+    brand: "Bewakoof",
+    name: "Conquer Strip Half Sleeve T-Shirts Black",
+    price: "499",
+    disPrice: "299",
+    color: "black",
+    pagelink: "#",
+  },
+  {
+    img: "https://images.bewakoof.com/t640/conquer-strip-half-sleeve-t-shirt-black-300965-1656141795-1.jpg",
+    brand: "Bewakoof",
+    name: "Conquer Strip Half Sleeve T-Shirts Black",
+    price: "499",
+    disPrice: "299",
+    color: "black",
+    pagelink: "#",
+  },
+  {
+    img: "https://images.bewakoof.com/t640/conquer-strip-half-sleeve-t-shirt-black-300965-1656141795-1.jpg",
+    brand: "Bewakoof",
+    name: "Conquer Strip Half Sleeve T-Shirts Black",
+    price: "499",
+    disPrice: "299",
+    color: "black",
+    pagelink: "#",
+  },
+  {
+    img: "https://images.bewakoof.com/t640/conquer-strip-half-sleeve-t-shirt-black-300965-1656141795-1.jpg",
+    brand: "Bewakoof",
+    name: "Conquer Strip Half Sleeve T-Shirts Black",
+    price: "499",
+    disPrice: "299",
+    color: "black",
+    pagelink: "#",
+  },
+  {
+    img: "https://images.bewakoof.com/t640/conquer-strip-half-sleeve-t-shirt-black-300965-1656141795-1.jpg",
+    brand: "Bewakoof",
+    name: "Conquer Strip Half Sleeve T-Shirts Black",
+    price: "499",
+    disPrice: "599",
+    color: "blue",
+    pagelink: "#",
+  },
+  {
+    img: "https://images.bewakoof.com/t640/conquer-strip-half-sleeve-t-shirt-black-300965-1656141795-1.jpg",
+    brand: "Bewakoof",
+    name: "Conquer Strip Half Sleeve T-Shirts Black",
+    price: "499",
+    disPrice: "199",
+    color: "yellow",
+    pagelink: "#",
+  },
+  {
+    img: "https://images.bewakoof.com/t640/conquer-strip-half-sleeve-t-shirt-black-300965-1656141795-1.jpg",
+    brand: "Bewakoof",
+    name: "Conquer Strip Half Sleeve T-Shirts Black",
+    price: "499",
+    disPrice: "299",
+    color: "black",
+    pagelink: "#",
+  },
+];
+
+updateCart(menPageData);
+function updateCart(arr) {
+  arr.forEach((ele) => {
+    //Make divs and all
+    let mainDiv = document.createElement("div");
+    let pagelink = document.createElement("a");
+    let bottomdiv = document.createElement("div");
+    let leftDiv = document.createElement("div");
+    let rightDiv = document.createElement("div");
+    let img = document.createElement("img");
+    let brand = document.createElement("p");
+    let name = document.createElement("p");
+    let disPrice = document.createElement("span");
+    let price = document.createElement("span");
+    let tribe = document.createElement("p");
+    let cotton = document.createElement("p");
+    let like = document.createElement("img");
+    //Assign values
+
+    pagelink.setAttribute("href", ele.pagelink);
+    img.setAttribute("src", ele.img);
+    brand.textContent = ele.brand;
+    name.textContent = ele.name;
+    disPrice.textContent = `₹${ele.disPrice}`;
+    price.textContent = ele.price;
+    tribe.textContent = "₹399 For TriBe Members";
+    cotton.textContent = "100% COTTON";
+    like.setAttribute("src", "./navbar/ic-web-head-wishlist.svg");
+
+    //counting
+    document.querySelector("#counting span").textContent = `(${arr.length})`;
+
+    //append
+    leftDiv.append(brand, name, disPrice, price, tribe, cotton);
+    rightDiv.append(like);
+    bottomdiv.append(leftDiv, rightDiv);
+    mainDiv.append(img, bottomdiv);
+    pagelink.append(mainDiv);
+    document.querySelector("#products").append(pagelink);
+  });
+}
+
+var colorFilter = document.querySelector("#color_filter");
+var brandFilter = document.querySelector("#brand_filter");
+var sort = document.querySelector("#sort select");
+colorFilter.addEventListener("change", (el) => {
+  let value = colorFilter.value;
+  //   console.log(value);
+  if (value == "") {
+    document.querySelector("#products").innerHTML = "";
+    updateCart(menPageData);
+  } else {
+    let fil = menPageData.filter((element) => {
+      return element.color == value;
+    });
+    document.querySelector("#products").innerHTML = "";
+    updateCart(fil);
+  }
+});
+
+brandFilter.addEventListener("change", (el) => {
+  let value = brandFilter.value;
+  //   console.log(value);
+  if (value == "") {
+    document.querySelector("#products").innerHTML = "";
+    updateCart(menPageData);
+  } else {
+    let fil = menPageData.filter((element) => {
+      return element.brand == value;
+    });
+    document.querySelector("#products").innerHTML = "";
+    updateCart(fil);
+  }
+});
+
+var sortArr = [...menPageData];
+
+sort.addEventListener("change", () => {
+  let value = sort.value;
+  if (value == "") {
+    document.querySelector("#products").innerHTML = "";
+    updateCart(menPageData);
+  } else if (value == "high_low") {
+    sortArr.sort((a, b) => {
+      if (a.disPrice > b.disPrice) {
+        return -1;
+      } else {
+        return 1;
+      }
+    });
+    document.querySelector("#products").innerHTML = "";
+    updateCart(sortArr);
+  } else if (value == "low_high") {
+    sortArr.sort((a, b) => {
+      if (a.disPrice > b.disPrice) {
+        return 1;
+      } else {
+        return -1;
+      }
+    });
+    document.querySelector("#products").innerHTML = "";
+    updateCart(sortArr);
+  }
+});
